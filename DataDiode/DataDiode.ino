168159b08f9230ab5330fe695d4191d682a7d820
Sven Seeberg <mail@sven-seeberg.de>
2018-11-12 01:01:09 +0100

    Arduino: performance optimization

--- DataDiode/DataDiode.ino
+++ DataDiode/DataDiode.ino
@@ -8,10 +8,14 @@ hd44780_I2Cexp lcd; // declare lcd object: auto locate & config exapander chip
 const int LCD_COLS = 16;
 const int LCD_ROWS = 2;
 
-char incomingByte = ' ';
 int x = 0;
 unsigned long counter = 0;
 unsigned int f_counter = 0;
+unsigned int f_counter_prev = 0;
+bool transfer = false;
+bool transfer_prev = false;
+bool update = false;
+
 void setup()
 {
   int status;
@@ -24,32 +28,49 @@ void setup()
   }
   Serial.begin(19200);
   lcd.print("Idle");
+  lcd.setCursor(0,1);
+  lcd.print(counter, DEC);
+  lcd.write("B");
+  lcd.setCursor(9,0);
+  lcd.print(f_counter, DEC);
+  lcd.write("F");
 }
 
 void loop() {
-  if (Serial.available() > 0) {
-    incomingByte = Serial.read();
-    Serial.println(incomingByte);
-    counter++;
-    x++;
-    if(incomingByte == char(1)) {
+  x++;
+  if (x>=10) {
+    x = 0;
+    if(transfer and not transfer_prev) {
       lcd.setCursor(0,0);
       lcd.write("Transfer");
-      lcd.setCursor(9,0);
-      f_counter++;
-      lcd.print(f_counter, DEC);
-      lcd.write("F");
-    } else if(incomingByte == char(4)) {
+    } else if (not transfer and transfer_prev) {
       lcd.setCursor(0,0);
       lcd.write("Idle    ");
+    }
+    if (f_counter != f_counter_prev) {
+      lcd.setCursor(9,0);
+      lcd.print(f_counter, DEC);
+      lcd.write("F");
+    }
+    if (update) {
+      update = false;
       lcd.setCursor(0,1);
       lcd.print(counter, DEC);
       lcd.write("B");
-    } else if (x >= 10) {
-      x = 0;
-      lcd.setCursor(0,1);
-      lcd.print(counter, DEC);
-      lcd.write("B");
+    }
+  }
+}
+
+void serialEvent() {
+  update = true;
+  while (Serial.available()) {
+    char inChar = (char)Serial.read();
+    counter++;
+    if (inChar == char(1)) {
+      transfer = true;
+      f_counter++;
+    } else if (inChar == char(4)) {
+      transfer = false;
     }
   }
 }
